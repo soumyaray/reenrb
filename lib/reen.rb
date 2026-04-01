737cbc7451d6bb3a2804e685914b09bb63f15d7e
Soumya Ray <soumya.ray@gmail.com>
2026-04-01 23:35:31 +0800

    refactor: renamer service in application layer

--- lib/reen.rb
+++ lib/reen.rb
@@ -6,7 +6,7 @@ require_relative "reen/domain/path_entry_list"
 require_relative "reen/domain/change"
 require_relative "reen/domain/changes"
 require_relative "reen/infrastructure/changes_file"
-require_relative "reen/services/reen"
+require_relative "reen/application/renamer"
 require_relative "reen/application/reen_cli"
 
 module Reen
